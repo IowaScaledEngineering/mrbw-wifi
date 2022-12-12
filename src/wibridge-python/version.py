5d74f5fab9931a98d0201696beeb90686b61374b
Nathan D. Holmes <ndholmes@fedex.com>
2022-12-12 12:56:11 -0700

    Automatically generating mrbwwifi-1_0_3.bin and mrbwwifi-init-1_0_3.h

--- src/wibridge-python/version.py
+++ src/wibridge-python/version.py
@@ -1,4 +1,4 @@
-_version_git_ = "9ecbc145d9c657e44f5f5062049bdcb6d4821ec4"
+_version_git_ = "9948879ac874d5e34915a4261289b868882a7745"
 _version_major_ = 1
 _version_minor_ = 0
-_version_delta_ = 2
+_version_delta_ = 3
